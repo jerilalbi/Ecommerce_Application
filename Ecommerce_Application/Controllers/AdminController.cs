c816d6bfdeb21c6f29f7bcfaca8151c9da2d26b2
Jeril Albi <devjerilalbi@gmail.com>
2025-10-09 14:27:07 +0530

    UI complete for admin

---
+++ Ecommerce_Application/Controllers/AdminController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ecommerce_Application.Controllers
+{
+    public class AdminController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Dashboard()
+        {
+            return PartialView("_Dashboard");
+        }
+
+        public ActionResult AddProduct() { 
+            return PartialView("_AddProduct");
+        }
+
+        public ActionResult Users()
+        {
+            return PartialView("_Users");
+        }
+
+        public ActionResult LogOut()
+        {
+            Session.Abandon();
+            return RedirectToAction("Login", "User");
+        }
+    }
+}
---
+++ Ecommerce_Application/Scripts/App/Dashboard.js
@@ -0,0 +1,33 @@
+﻿$(document).ready(function () {
+    const contentPlaceholder = $('#contentHolder');
+    const navLinks = $('.admin-btn');
+
+    function loadContent(url) {
+        $.ajax({
+            url: url,
+            type: 'GET',
+            success: function (response) {
+                contentPlaceholder.html(response);
+            },
+            error: function () {
+                contentPlaceholder.html('<div class="alert alert-danger">Error loading content. Please try again.</div>');
+            }
+        });
+    }
+
+    navLinks.on('click', function (e) {
+        e.preventDefault();
+        const url = $(this).data('url');
+
+        console.log("button clicked");
+
+        navLinks.removeClass('active');
+        $(this).addClass('active');
+
+        if (url) {
+            loadContent(url);
+        }
+    });
+
+    $('.sidebar a[data-url="/Admin/Dashboard"]').click();
+});
